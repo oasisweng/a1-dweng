33905a82bae9c867b95de4f387d70531d2eeac9f
oasisweng <dingzhong.weng@lycheestudio.co.uk>
2014-10-30 19:55:10 +0000

    fix line 58 in index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
         $stmt->bindValue(3, $date);
-        $stmt->bindValue(4, $company_name)
+        $stmt->bindValue(4, $company_name);
         $stmt->execute();
     }
     catch(Exception $e) {
